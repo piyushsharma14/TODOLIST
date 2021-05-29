4f937c9ebd98ab0c697d17502cc97e99ce563c59
piyushsharma14 <69628515+piyushsharma14@users.noreply.github.com>
2021-05-29 16:27:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,14 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
-import App from "./component/TODO/App";
-import Details from "./component/Profile/details";
-import User from "./component/Login/Login";
-import Register from "./component/Register/Register";
 import store from "./store";
-import PrivateRoute from "./privaterouter";
-
-const Index = () => {
-  <Details />;
-  return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={User} />
-          <PrivateRoute exact path='/status' component={App} />
-          <Route exact path="/register" component={Register} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
-};
+import App from "./App"
 
 store.subscribe(() => store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Index />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
